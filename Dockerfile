644b7ca400129fdf813d0a905da2bd0abd2690ef
Yui Nishizawa <yui@exaptive.com>
2022-09-17 12:49:54 +0900

    First test checkin

--- Dockerfile
+++ Dockerfile
@@ -3,14 +3,31 @@ FROM ubuntu:focal
 # step: raw image is minimized
 RUN yes | unminimize
 
+# note: disable apt interaction
+# If not disabled, apt may require input
+# e.g.
+# 	Please select the geographic area in which you live. Subsequent configuration questions will narrow this down by presenting a list of cities, representing
+# 	the time zones in which they are located.
+# 	1. Africa   3. Antarctica  5. Arctic  7. Atlantic  9. Indian    11. SystemV  13. Etc
+# 	2. America  4. Australia   6. Asia    8. Europe    10. Pacific  12. US
+# Ref: https://stackoverflow.com/questions/59692797/how-to-fill-user-input-for-interactive-command-for-run-command
+ENV DEBIAN_FRONTEND noninteractive
+
 # step: install basic packages
 RUN apt update && apt upgrade -y && apt update && \
   apt install -y software-properties-common \
-   man less curl sudo git \
-   vim neovim 
+   man less sudo rsync curl \
+	 python3-dev cmake git vim neovim 
 
 # step: add user
 RUN useradd -ms /bin/bash guest
+# sudoer
+RUN usermod -aG sudo guest
+# set password "guest"
+# RUN echo "guest:guest" | chpasswd
+# no password (for sudo)
+RUN passwd --delete guest
+
 USER guest
 
 WORKDIR /home/guest
---
+++ test.sh
@@ -0,0 +1,92 @@
+#!/bin/bash
+set -eu -o pipefail
+
+# shellcheck disable=SC2034
+__dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+# shellcheck disable=SC2034
+__file="${__dir}/$(basename "${BASH_SOURCE[0]}")"
+
+# Usage
+usage() {
+	cat <<END
+Test
+END
+}
+
+DOCKERFILE="$__dir/Dockerfile"
+IMAGENAME="yuitoolstest"
+
+USAGE_EXITCODE=200
+# shellcheck disable=SC2120
+usageAndExit() {
+	usage
+	if [[ $# = 1 ]]; then exit "$1"; else exit "$USAGE_EXITCODE"; fi
+}
+
+cleanup() {
+	code="$?"
+	if [[ "$code" -eq 0 ]]
+	then
+		echo "Done"
+	elif [[ "$code" -ne "$USAGE_EXITCODE" ]]
+	then
+		# case: error other than usage message
+		onError
+	fi
+	exit "$code"
+}
+onError() {
+	echo "Error"
+}
+# cleanup: on script exit
+# Both success and fail
+trap cleanup EXIT RETURN
+
+declare DEBUG
+main() {
+	# step: parse options
+	while [[ $# -gt 0 ]]
+	do
+		echo "a $1 b"
+		case "$1" in
+			--debug)
+				DEBUG=1
+				shift
+				;;
+			# case: error handling
+			-*)
+				echo "Unknown option $1" >&2
+				usageAndExit
+				;;
+			# case: implicit start of positional argument
+			*)
+				break
+				;;
+			esac
+		shift
+	done
+
+	runTest
+}
+
+runTest() {
+	cd "$__dir"
+	docker build -f "$DOCKERFILE" -t "$IMAGENAME" .
+
+	# note: using '( expr ) || true'
+	# To
+	docker run -ti  \
+		-v "$__dir":"/home/guest/.yui_tools" \
+		"$IMAGENAME" \
+		bash -c "( cd .yui_tools \
+			&& bash ./setup.sh \
+			&& echo 'Testing neovim. Hit enter.' && read \
+			&& nvim \
+			&& nvim -c PlugInstall \
+			&& vim ; ) || true \
+			&& echo 'Done tests. Entering shell to debug/play around.' \
+			&& bash"
+			# && pushd ~/.vim/plugged/YouCompleteMe && ./install.py --all && popd
+}
+
+(main "$@")
