a52f0a724e526549a990081289aea36e4a8c1cee
Yui Nishizawa <yui.nishizawa@gmail.com>
2025-10-04 05:19:58 +0900

    October updates: - bashrc improvements - psqlrc: use temp tables

--- dotfiles/bashrc/bashrc_fzf.sh
+++ dotfiles/bashrc/bashrc_fzf.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+
+## --preview: show preview up to `head 100` lines
 FZF_DEFAULT_OPTS="--preview='head -100 {}' --bind 'ctrl-alt-k:preview-up,ctrl-alt-j:preview-down' --history=$HOME/.fzf.history"
 if  which fzf > /dev/null;
 then
